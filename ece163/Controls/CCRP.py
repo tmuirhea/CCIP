b8070bf057d7df92f6e2b2835bd7f58f88dea56a
AlexSchulte45 <ce.alex.schulte.gmail.com>
2021-06-03 20:19:06 -0700

    some changes to CCIP + CCIP update

--- ece163/Controls/CCRP.py
+++ ece163/Controls/CCRP.py
@@ -21,9 +21,36 @@ class PayloadAerodynamicModel:
         pe = self.state.pe + self.state.v * time
         pd = self.state.pd + self.state.w * time
         return pn,pe,pd
-    def calculateTOF(self,mass, planArea, cofDrag):
-        z = [self.state.pd]
-        v = [self.state.w]
+    def Update(self)
+        
+class CCIP:
+    def __init__(self,mass,targetx,targety,targetz):
+        self.closed = VCLC.VehicleClosedLoopControl()
+        self.payload = PayloadAerodynamicModel()
+        self.dT = VPC.dT
+        self.TOF = 0
+        self.mass = mass;
+        self.area = 1;
+        self.cofDrag = .5
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.targetx = targetx
+        self.targety = targety
+        self.targetz = targetz
+        self.isreleased = 0
+    def createReference(self):
+        x,y = self.targety - self.closed.getVehicleState().pe ,self.targetx - self.closed.getVehicleState().pn
+        course  = math.pi/2 - math.atan2(y,x)
+        reference = ctrl.referenceCommands(courseCommand=course)
+        return reference
+    
+    def releasePayload(self,state,mass,dT):
+        self.payload = PayloadAerodynamicModel(state.pn,state.pe,state.pd,state.u,state.v,state.w,mass,dT)
+        
+    def calculateTOF(self,state, mass, planArea, cofDrag, dT):
+        z = [state.pd]
+        v = [state.w]
         i = 0
         #assuming 1m radius sphere
         while z[i] < 0:
@@ -32,13 +59,18 @@ class PayloadAerodynamicModel:
             i += 1
         return i * self.dT
 
-class CCIP:
-    def __init__(self):
-        self.closed = VCLC.VehicleClosedLoopControl()
-        self.payload = PayloadAerodynamicModel()
-    def createReference(self):
-        x,y = self.payload.state.pe - self.closed.getVehicleState().pe ,self.payload.state.pn - self.closed.getVehicleState().pn
-        course  = math.pi/2 - math.atan2(y,x)
-        reference = ctrl.referenceCommands(courseCommand=course)
-        return reference
-
+    def Update(self,RefCommand):
+        self.VCLC.Update(RefCommand)
+        self.TOF = calculateTOF(self.closed.VAM.VDM.state, self.mass,self.area,self.cofDrag,self.dT)
+        self.x,self.y,self.z = self.payload.calculateFuturePos(self.TOF)
+        #if targets line up
+        if ((self.x- self.targetx) < .5 ): #if x coordinates withing half meter
+            if ((self.y- self.targety) < .5 ):
+                if ((self.z- self.targetz) < .5 ):
+                    releasePayload(self.closed.VAM.VDM.state,self.mass,self.dT)
+                    self.isreleased = 1
+        if (self.isreleased == 1): #if payload is released check for impact
+            #check for impact
+        refCommand = self.createReference()
+        return RefCommand
+        
