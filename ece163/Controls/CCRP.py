5ebfc885cca7fc934bc80f36d0898e4cbad56f38
Kevin Jesubalan <kjesubal@ucsc.edu>
2021-06-05 16:49:53 -0700

    fixed tof to calculated speeds of every direction and removed pos cuz it was useless

--- ece163/Controls/CCRP.py
+++ ece163/Controls/CCRP.py
@@ -7,7 +7,8 @@ import ece163.Containers.Controls as ctrl
 
 class PayloadAerodynamicModel:
     # default assumes sphere of radius 1m and slow speeds for coefficient of drag
-    def __init__(self, pn=0.0, pe=0.0, pd=0.0, u=0.0, v=0.0, w=0.0, dT=VPC.dT, mass=10, planArea=math.pi, cofDrag=0.5,released = 0):
+    def __init__(self, pn=0.0, pe=0.0, pd=0.0, u=0.0, v=0.0, w=0.0, dT=VPC.dT,  planArea=math.pi, cofDrag=0.5,mass=10,
+                 released=0):
         self.state = States.vehicleState()
         self.state.pn = pn
         self.state.pe = pe
@@ -20,27 +21,34 @@ class PayloadAerodynamicModel:
         self.mass = mass
         self.dT = dT
         self.released = released
-    def calculateFuturePos(self, time,state,dot):
-        #very simplistic does not account for wind or drag as is.
+
+    def calculateFuturePos(self, time, state, dot):
+        # very simplistic does not account for wind or drag as is.
         ## should pd be included in here?
         ## I wanted to do futurepos in the TOF while loop but was unsure of converting u,v to pn,pe
         pn = state.pn + (dot.u * time)
         pe = state.pe + (dot.v * time)
         return pn, pe
+
     def reset(self):
         self.state = States.vehicleState()
+
     def Update(self):
         if self.released == 1:
             self.state.pn = self.state.pn + self.state.u * self.dT
             self.state.pe = self.state.pe + self.state.v * self.dT
             self.state.pd = self.state.pd + self.state.w * self.dT
-            if(self.state.pd < 0.0):
+            if (self.state.pd < 0.0):
                 magnitude = math.hypot(self.state.u, self.state.v, self.state.w)  # needed for calculating drag
-                #one timestep of updating speeds which is just drag and gravity in the case of the z direction
-                self.state.u = self.state.u - self.dT * (VPC.rho * self.planArea * self.cofDrag * magnitude * self.state.u) / (self.mass*2)
-                self.state.v = self.state.v - self.dT * (VPC.rho * self.planArea * self.cofDrag * magnitude * self.state.v) / (self.mass*2)
-                self.state.w = self.state.w + self.dT * (VPC.g0 - (VPC.rho * self.planArea * self.cofDrag * magnitude * self.state.w) / (self.mass*2))
-            else :
+                # one timestep of updating speeds which is just drag and gravity in the case of the z direction
+                self.state.u = self.state.u - self.dT * (
+                        VPC.rho * self.planArea * self.cofDrag * magnitude * self.state.u) / (self.mass * 2)
+                self.state.v = self.state.v - self.dT * (
+                        VPC.rho * self.planArea * self.cofDrag * magnitude * self.state.v) / (self.mass * 2)
+                self.state.w = self.state.w + self.dT * (
+                        VPC.g0 - (VPC.rho * self.planArea * self.cofDrag * magnitude * self.state.w) / (
+                        self.mass * 2))
+            else:
                 self.state.u = 0.0
                 self.state.v = 0.0
                 self.state.w = 0.0
@@ -49,8 +57,9 @@ class PayloadAerodynamicModel:
         ### Alex: I think wind is calulated
         ### in update forces. I think we just need to call it
 
+
 class CCIP:
-    def __init__(self, targetx = 100.0, targety = 100.0, targetz = 0.0):
+    def __init__(self, targetx=100.0, targety=100.0, targetz=0.0):
         self.closed = VCLC.VehicleClosedLoopControl()
         self.payload = PayloadAerodynamicModel()
         self.dT = VPC.dT
@@ -62,14 +71,17 @@ class CCIP:
         self.targety = targety
         self.targetz = targetz
         self.isreleased = 0
-    #for acquiring a new target
+
+    # for acquiring a new target
     def acquireTarget(self, targetx, targety, targetz):
         self.targetx = targetx
         self.targety = targety
         self.targetz = targetz
+
     def getTarget(self):
-        return self.targetx,self.targety,self.targetz
-    #reset should set the payload back to nothing, set closed back to
+        return self.targetx, self.targety, self.targetz
+
+    # reset should set the payload back to nothing, set closed back to
     # intial state,target to intital. 
     def reset(self):
         self.closed.reset()
@@ -79,39 +91,52 @@ class CCIP:
         self.targetx = 10.0
         self.targety = 10.0
         self.targetz = 0.0
-        
-    #added some state setters and getters
-    def setVehicleState(self,state):
+
+    # added some state setters and getters
+    def setVehicleState(self, state):
         self.closed.VAM.vehicle.state = state
+
     def getVehicleState(self):
         return self.closed.VAM.vehicle.state
-    
+
     def createReference(self):
         x, y = self.targety - self.closed.getVehicleState().pe, self.targetx - self.closed.getVehicleState().pn
         course = math.pi / 2 - math.atan2(y, x)
-        reference = ctrl.referenceCommands(courseCommand=course,altitudeCommand = 1000)
+        reference = ctrl.referenceCommands(courseCommand=course, altitudeCommand=1000)
         return reference
 
     ##added variables here to allow for releasing different payloads
-    def releasePayload(self, mass=10, area=math.pi, cofDrag=0.5):
+    def releasePayload(self, area=math.pi, cofDrag=0.5, mass=10):
         state = self.closed.getVehicleState()
         dot = self.closed.VAM.vehicle.dot
         self.payload = PayloadAerodynamicModel(state.pn, state.pe, state.pd, dot.pn, dot.pe, dot.pd, self.dT, mass,
                                                area, cofDrag, 1)
-    def calculateTOF(self, state,planArea,cofDrag,mass):
+
+    def calculateTOF(self, state, dot, planArea, cofDrag, mass):
+        x = [state.pn]
+        y = [state.pe]
         z = [state.pd]
-        
-        v = [state.w]
+        u = [dot.pn]
+        v = [dot.pe]
+        w = [dot.pd]
+
         i = 0
         while z[i] < 0:
-            magnitude = math.hypot(state.u, state.v, v[i])  # needed for calculating drag
-            v.append(v[i] + self.dT * (
-                        VPC.g0 - (VPC.rho * planArea * cofDrag * magnitude * v[i]) / (mass*2)))
-            z.append(z[i] + self.dT * v[i])
+            magnitude = math.hypot(u[i], v[i], w[i])  # needed for calculating drag
+            u.append(u[i] - self.dT * (VPC.rho * planArea * cofDrag * magnitude * u[i]) / (mass * 2))
+            v.append(v[i] - self.dT * (VPC.rho * planArea * cofDrag * magnitude * v[i]) / (mass * 2))
+            w.append(w[i] + self.dT * (
+                    VPC.g0 - (VPC.rho * planArea * cofDrag * magnitude * w[i]) / (mass * 2)))
+
+            x.append(x[i] + self.dT * u[i])
+            y.append(u[i] + self.dT * v[i])
+            z.append(z[i] + self.dT * w[i])
             i += 1
-        return i * self.dT
-    #IsImapcted returns 1/0 if payload intersects target
+        return x[i], z[i]
+
+    # IsImapcted returns 1/0 if payload intersects target
     def isImpacted(self):
+
         if abs(self.payload.state.pn - self.targetx) < 10:
             if abs(self.payload.state.pe - self.targety) < 10:
                 if abs(self.payload.state.pd - self.targetz) < 1:
@@ -119,10 +144,11 @@ class CCIP:
                     print(self.payload.state.pe)
                     print(self.payload.state.pd)
                     return 1
+
     def Update(self, RefCommand):
+
         self.closed.Update(RefCommand)
-        self.TOF = self.calculateTOF(self.closed.VAM.vehicle.state,math.pi,.5,25)
-        self.x, self.y = self.payload.calculateFuturePos(self.TOF, self.closed.VAM.vehicle.state, self.closed.VAM.vehicle.dot)
+        self.x, self.y = self.calculateTOF(self.closed.VAM.vehicle.state, self.closed.VAM.vehicle.dot, math.pi, .5, 25)
         # if targets line up
 
         if (abs(self.x - self.targetx) < 5):  # if x coordinates withing half meter
@@ -132,9 +158,9 @@ class CCIP:
                     print("released")
         if (self.payload.released == 1):  # if payload is released do an Update() amd check for impact
             self.payload.Update()
-            
+
             Impact = self.isImpacted()
-            #if impact "delete" payload and print
+            # if impact "delete" payload and print
             if Impact == 1:
                 print("IMPACT!!!!\n")
         RefCommand = self.createReference()
